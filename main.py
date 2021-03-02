151b5f965047cb1588da182e9e05ea0ba145094f
WiseKun <66040816+WiseKun@users.noreply.github.com>
2021-03-02 20:02:07 +0000

    Import dataset

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import csv
+import random
 
+# Opens the 'insurance.csv' file in read mode as 'dataset'
+with open('insurance.csv', 'r') as dataset:
+  dataset_dict = list(csv.DictReader(dataset))
+  dataset_length = len(dataset_dict)
+
+  # Prints a random record from the dataset
+  print('Here is a random record: {}\n'.format(dataset_dict[random.randint(0, dataset_length)]))
