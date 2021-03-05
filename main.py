2890205c1cb7cb35c69d815df1e1a7c541991960
Mustafa A <66040816+WiseKun@users.noreply.github.com>
2021-03-05 21:46:00 +0000

    Clean code

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
 import csv
 import random
 
-# Create new class to convert each piece of data to a more useable format
-class InsuranceRecord:
-  def __init__(self, age, sex, bmi, children, smoker, region, charges):
-    self.age = int(age)
-    self.sex = 0 if sex=='female' else 1
-    self.bmi = float(bmi)
-    self.children = int(children)
-    self.smoker = True if smoker=='yes' else False
-    self.region = region
-    self.charges = round(float(charges), 2)
-
-  # Returns the newly made dictionary
-  def return_record(self):
-    return {'Age': self.age, 'Sex': self.sex, 'BMI': self.bmi, 'Children': self.children, 'Smoker': self.smoker, 'Region': self.region, 'Charges': self.charges}
+# Function that converts data to more useable format
+def change_data(data):
+  age = int(data['age'])
+  sex = 0 if data['sex'] == 'female' else 1
+  bmi = float(data['bmi'])
+  children = int(data['children'])
+  smoker = True if data['smoker'] == 'yes' else False
+  charges = round(float(data['charges']), 2)
+  return {'Age': age, 'Sex': sex, 'BMI': bmi, 'Children': children, 'Smoker': smoker, 'Region': data['region'], 'Charges': charges}
 
 def analyse_smoker(dataset):
   # Finds the average cost
-  smoker_total = 0
-  num_of_smoker = 0
-  non_smoker_total = 0
-  num_of_non_smoker = 0
+  smoker_total, num_of_smoker, non_smoker_total, num_of_non_smoker = 0, 0, 0, 0
   # Loop through every record in the dataset
   for record in dataset:
     smoker = record['Smoker']
@@ -39,28 +31,25 @@ def analyse_smoker(dataset):
   return smoker_average, non_smoker_average
 
 def analyze_bmi(dataset):
-  # Find the average cost
-  underweight_total = 0
-  num_of_underweight = 0
-  healthy_total = 0
-  num_of_healthy = 0
-  overweight_total = 0
-  num_of_overweight = 0
-  obese_total = 0
-  num_of_obese = 0
+  # Finds the average cost
+  underweight_total, num_of_underweight, healthy_total, num_of_healthy, overweight_total, num_of_overweight, obese_total, num_of_obese = 0, 0, 0, 0, 0, 0, 0, 0
   # Loop through every record in the dataset
   for record in dataset:
     bmi = record['BMI']
     charges = record['Charges']
+    # If the person is underweight
     if bmi < 18.5:
       underweight_total += charges
       num_of_underweight += 1
+    # Else if the person is healthy weight
     elif bmi < 24.9:
       healthy_total += charges
       num_of_healthy += 1
+    # Else if the person is overweight
     elif bmi < 29.9:
       overweight_total += charges
       num_of_overweight += 1
+    # Else the person is obese
     else:
       obese_total += charges
       num_of_obese += 1
@@ -70,18 +59,20 @@ def analyze_bmi(dataset):
   overweight_average = round(overweight_total/num_of_overweight, 2)
   obese_average = round(obese_total/num_of_obese, 2)
 
+  # Return the calculated averages
   return underweight_average, healthy_average, overweight_average, obese_average
 
-# Opens the 'insurance.csv' file in read mode as 'dataset'
+# Opens the 'insurance.csv' file in read mode as dataset
 with open('insurance.csv', 'r') as dataset:
   dataset_dict = list(csv.DictReader(dataset))
   dataset_length = len(dataset_dict)
 
-  # Convert records to instances of the class 'InsuranceRecord'
+  # Convert records a more useable format
   new_dataset = []
   for i in range(dataset_length):
     cur = dataset_dict[i]
-    new_dataset.append(InsuranceRecord(cur['age'], cur['sex'], cur['bmi'], cur['children'], cur['smoker'], cur['region'], cur['charges']).return_record())
+    new_cur = change_data(cur)
+    new_dataset.append(new_cur)
 
   # Prints a random record from the dataset
   print('Here is a random record: {}\n'.format(new_dataset[random.randint(0, dataset_length)]))
@@ -95,4 +86,3 @@ with open('insurance.csv', 'r') as dataset:
   underweight_average, healthy_average, overweight_average, obese_average = analyze_bmi(new_dataset)
   print('Average cost for underweight: ${}\nAverage cost for healthy: ${}\nAverage cost for overweight: ${}\nAverage cost for obese: ${}\n'.format(underweight_average, healthy_average, overweight_average, obese_average))
   # This shows us that weight has a very big role in determining medical insurance cost. The lower the weight, the lower the cost.
- 
